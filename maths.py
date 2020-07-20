83d5ed50544a80288e752ff84d54c5434d61336a
maxstewart0 <maxstewart0@gmail.com>
2020-07-20 16:14:08 +1000

    added add function

--- maths.py
+++ maths.py
@@ -3,7 +3,10 @@ def divide(num1, num2):
 def multiply(num1,num2):
     return num1*num2
 
+def add(num1,num2):
+    return num1+num2
 a = 1
 b = 2
 print("Divide: ", divide(a,b))
 print("Multiply: ",multiply(a,b))
+print("Add: ",add(a,b))
