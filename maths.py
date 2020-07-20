05bb5d0c07a0d14629395af69ffa7edb09a80f1c
maxstewart0 <maxstewart0@gmail.com>
2020-07-20 16:01:49 +1000

    adding a multiplication function

--- maths.py
+++ maths.py
@@ -1,4 +1,7 @@
 def divide(num1, num2):
     return num1/num2
+def multiply(num1,num2):
+    return num1*num2
 
-print(divide(1,2))
+print("Divide: " + divide(1,2))
+print("Multiply: "+ multiply(1,2))
